ad3ff187d78fe72f80b0d2732bd170ab9c916597
Tuan Amith <ymuraad@gmail.com>
2017-10-12 11:21:13 +0530

    user login and registration

---
+++ application/views/User/userRegistration.php
@@ -0,0 +1,58 @@
+<html>
+
+<head>
+    <title>User Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+        .vertical-center {
+            min-height: 100%;
+            display: flex;
+            align-items: center;
+        }
+
+        .form-horizontal .control-label{
+            /* text-align:right; */
+            text-align:left;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="vertical-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-sm-offset-3">
+                <div class="col-sm-12" style="padding-bottom: 20px">
+                    <h1>SignUp</h1>
+                </div>
+                <!--                <div class="img-circle">-->
+                <!--                    <img src="#" alt="Avatar" class="img-circle">-->
+                <!--                </div>-->
+                <?php echo form_open('user/userRegistration', 'class="form-horizontal"'); ?>
+                <!--                <form method="post" action="user/userAuthenticate" class="form-horizontal">-->
+                <div class="form-group">
+                    <label class="control-label col-sm-3" for="username">Username : </label>
+                    <div class="col-sm-9">
+                        <input class="form-control" name="username" placeholder="Sam1990" id="username">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-sm-3" for="password" >Password : </label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" placeholder="xxxxxx" id="password">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <button type="submit" class="btn btn-default">SignUp</button>
+                    </div>
+                </div>
+                <?php form_close() ?>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+
+</html>
