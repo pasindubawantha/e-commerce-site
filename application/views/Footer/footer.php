450f79af63d8d0895c62e61422097494a62c0a53
Pasindu Perera <pasindubawantha@gmail.com>
2017-10-19 09:14:07 +0530

    restarting codebase

--- application/views/Footer/footer.php
+++
@@ -1,7 +0,0 @@
-	</div>
-	</div>
-	<script src="http://localhost:8002/assests/js/custom.js"></script>
-	<script src="http://localhost:8002/assests/js/jquery-3.2.1.min.js"></script>
-	<script src="http://localhost:8002//assests/js/bootstrap.min.js"></script>
-	</body>
-</html>
--- application/views/checkDatabaseview.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-
-	foreach($query->result() as $row){
-		echo $row->Tables_in_ecommerce . "\n <br>";
-	}
-?>
-
