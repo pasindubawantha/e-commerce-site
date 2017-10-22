8a25c1220fb0945ba93fdaeceb8d348aec5161f0
Pasindu Perera <pasindubawantha@gmail.com>
2017-10-22 21:53:21 +0530

    new database uodate database before running

--- Database/ecommerce.sql
+++ Database/ecommerce.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 01, 2017 at 03:10 PM
+-- Generation Time: Oct 22, 2017 at 04:22 PM
 -- Server version: 5.7.19
--- PHP Version: 7.0.22
+-- PHP Version: 7.0.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -30,18 +30,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `Category` (
   `id` int(11) NOT NULL,
-  `name` varchar(45) DEFAULT NULL
+  `name` varchar(45) DEFAULT NULL,
+  `parent_category` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `Category`
 --
 
-INSERT INTO `Category` (`id`, `name`) VALUES
-(1001, 'category1'),
-(1002, 'category2'),
-(1003, 'category3'),
-(1004, 'category4');
+INSERT INTO `Category` (`id`, `name`, `parent_category`) VALUES
+(1001, 'cat1', NULL),
+(1002, 'cat2', NULL);
 
 -- --------------------------------------------------------
 
@@ -53,7 +52,7 @@ CREATE TABLE `Customer` (
   `id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `date_joined` varchar(45) DEFAULT NULL,
-  `User_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -69,23 +68,8 @@ CREATE TABLE `Item` (
   `price` int(11) DEFAULT NULL,
   `units` varchar(15) DEFAULT NULL,
   `rank` int(11) DEFAULT NULL,
-  `Category_id` int(11) NOT NULL
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `Item`
---
-
-INSERT INTO `Item` (`id`, `name`, `description`, `price`, `units`, `rank`, `Category_id`) VALUES
-(2001, 'item1', 'item1 description', 10, 'no', 1, 1001),
-(2002, 'item2', 'item2 description', 20, 'kg', 2, 1001),
-(2003, 'item3', 'item3 decription', 30, 'g', 3, 1001),
-(2004, 'item4', 'item4 decription', 40, 'units', 4, 1001),
-(2005, 'item5', 'item5 description', 50, 'kg', 5, 1002),
-(2006, 'item6', 'item6 description', 60, 'g', 9, 1002),
-(2007, 'item7', 'item7 description', 70, 'units', 8, 1002),
-(2008, 'item8', 'item8 description', 80, 'g', 7, 1003),
-(2009, 'item9', 'item9 description', 90, 'kg', 6, 1004);
 
 -- --------------------------------------------------------
 
@@ -96,19 +80,19 @@ INSERT INTO `Item` (`id`, `name`, `description`, `price`, `units`, `rank`, `Cate
 CREATE TABLE `Order` (
   `id` int(11) NOT NULL,
   `state` enum('pending','confirmed','paied','delivered') DEFAULT NULL,
-  `Customer_id` int(11) NOT NULL,
-  `Shipper_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL,
+  `shipper_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `OrderQutedPrice`
+-- Table structure for table `Order_quoted_price`
 --
 
-CREATE TABLE `OrderQutedPrice` (
+CREATE TABLE `Order_quoted_price` (
   `Item_id` int(11) NOT NULL,
-  `Order_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `price` int(11) DEFAULT NULL,
   `quantity` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -125,8 +109,8 @@ CREATE TABLE `PersonalInfo` (
   `address` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `rating` varchar(45) DEFAULT NULL,
-  `Customer_id` int(11) NOT NULL,
-  `Supplier_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL,
+  `supplier_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -144,6 +128,25 @@ CREATE TABLE `Shipper` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `Site`
+--
+
+CREATE TABLE `Site` (
+  `name` varchar(45) NOT NULL,
+  `data` json DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `Site`
+--
+
+INSERT INTO `Site` (`name`, `data`) VALUES
+('registration_email', '{\"message\": \"custom message\", \"siteURL\": \"http://localhost:8002/\", \"subject\": \"Registration Ecommerce\", \"from_name\": \"ecommerce site\", \"from_email\": \"register@ecommerce.com\"}'),
+('site', '{\"title\": \"Site Title\", \"twitterURL\": null, \"youtubeURL\": null, \"facebookURL\": \"#\", \"instagramURL\": null, \"searchKeywords\": \"ecommerce site\"}');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `Supplier`
 --
 
@@ -151,7 +154,7 @@ CREATE TABLE `Supplier` (
   `id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `User_id` int(11) NOT NULL
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -162,8 +165,11 @@ CREATE TABLE `Supplier` (
 
 CREATE TABLE `User` (
   `id` int(11) NOT NULL,
-  `user_name` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL
+  `user_name` varchar(45) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `password_salt` varchar(64) NOT NULL,
+  `verification_code` varchar(64) NOT NULL,
+  `verified` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -174,45 +180,46 @@ CREATE TABLE `User` (
 -- Indexes for table `Category`
 --
 ALTER TABLE `Category`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_Category_Category1_idx` (`parent_category`);
 
 --
 -- Indexes for table `Customer`
 --
 ALTER TABLE `Customer`
-  ADD PRIMARY KEY (`id`,`User_id`),
-  ADD KEY `fk_Customer_User1_idx` (`User_id`);
+  ADD PRIMARY KEY (`id`,`user_id`),
+  ADD KEY `fk_Customer_User1_idx` (`user_id`);
 
 --
 -- Indexes for table `Item`
 --
 ALTER TABLE `Item`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Item_Category_idx` (`Category_id`);
+  ADD KEY `fk_Item_Category_idx` (`category_id`);
 
 --
 -- Indexes for table `Order`
 --
 ALTER TABLE `Order`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_Order_Customer1_idx` (`Customer_id`),
-  ADD KEY `fk_Order_Shipper1_idx` (`Shipper_id`);
+  ADD KEY `fk_Order_Customer1_idx` (`customer_id`),
+  ADD KEY `fk_Order_Shipper1_idx` (`shipper_id`);
 
 --
--- Indexes for table `OrderQutedPrice`
+-- Indexes for table `Order_quoted_price`
 --
-ALTER TABLE `OrderQutedPrice`
-  ADD PRIMARY KEY (`Item_id`,`Order_id`),
-  ADD KEY `fk_Item_has_Order_Order1_idx` (`Order_id`),
+ALTER TABLE `Order_quoted_price`
+  ADD PRIMARY KEY (`Item_id`,`order_id`),
+  ADD KEY `fk_Item_has_Order_Order1_idx` (`order_id`),
   ADD KEY `fk_Item_has_Order_Item1_idx` (`Item_id`);
 
 --
 -- Indexes for table `PersonalInfo`
 --
 ALTER TABLE `PersonalInfo`
-  ADD PRIMARY KEY (`id`,`Customer_id`,`Supplier_id`),
-  ADD KEY `fk_PersonalInfo_Customer1_idx` (`Customer_id`),
-  ADD KEY `fk_PersonalInfo_Supplier1_idx` (`Supplier_id`);
+  ADD PRIMARY KEY (`id`,`customer_id`,`supplier_id`),
+  ADD KEY `fk_PersonalInfo_Customer1_idx` (`customer_id`),
+  ADD KEY `fk_PersonalInfo_Supplier1_idx` (`supplier_id`);
 
 --
 -- Indexes for table `Shipper`
@@ -221,60 +228,82 @@ ALTER TABLE `Shipper`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `Site`
+--
+ALTER TABLE `Site`
+  ADD PRIMARY KEY (`name`);
+
+--
 -- Indexes for table `Supplier`
 --
 ALTER TABLE `Supplier`
-  ADD PRIMARY KEY (`id`,`User_id`),
-  ADD KEY `fk_Supplier_User1_idx` (`User_id`);
+  ADD PRIMARY KEY (`id`,`user_id`),
+  ADD KEY `fk_Supplier_User1_idx` (`user_id`);
 
 --
 -- Indexes for table `User`
 --
 ALTER TABLE `User`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_name_UNIQUE` (`user_name`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- Constraints for dumped tables
 --
 
 --
+-- Constraints for table `Category`
+--
+ALTER TABLE `Category`
+  ADD CONSTRAINT `fk_Category_Category1` FOREIGN KEY (`parent_category`) REFERENCES `Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
 -- Constraints for table `Customer`
 --
 ALTER TABLE `Customer`
-  ADD CONSTRAINT `fk_Customer_User1` FOREIGN KEY (`User_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Customer_User1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Item`
 --
 ALTER TABLE `Item`
-  ADD CONSTRAINT `fk_Item_Category` FOREIGN KEY (`Category_id`) REFERENCES `Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Item_Category` FOREIGN KEY (`category_id`) REFERENCES `Category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Order`
 --
 ALTER TABLE `Order`
-  ADD CONSTRAINT `fk_Order_Customer1` FOREIGN KEY (`Customer_id`) REFERENCES `Customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Order_Shipper1` FOREIGN KEY (`Shipper_id`) REFERENCES `Shipper` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Order_Customer1` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_Order_Shipper1` FOREIGN KEY (`shipper_id`) REFERENCES `Shipper` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
--- Constraints for table `OrderQutedPrice`
+-- Constraints for table `Order_quoted_price`
 --
-ALTER TABLE `OrderQutedPrice`
+ALTER TABLE `Order_quoted_price`
   ADD CONSTRAINT `fk_Item_has_Order_Item1` FOREIGN KEY (`Item_id`) REFERENCES `Item` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_Item_has_Order_Order1` FOREIGN KEY (`Order_id`) REFERENCES `Order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Item_has_Order_Order1` FOREIGN KEY (`order_id`) REFERENCES `Order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `PersonalInfo`
 --
 ALTER TABLE `PersonalInfo`
-  ADD CONSTRAINT `fk_PersonalInfo_Customer1` FOREIGN KEY (`Customer_id`) REFERENCES `Customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_PersonalInfo_Supplier1` FOREIGN KEY (`Supplier_id`) REFERENCES `Supplier` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_PersonalInfo_Customer1` FOREIGN KEY (`customer_id`) REFERENCES `Customer` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_PersonalInfo_Supplier1` FOREIGN KEY (`supplier_id`) REFERENCES `Supplier` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Constraints for table `Supplier`
 --
 ALTER TABLE `Supplier`
-  ADD CONSTRAINT `fk_Supplier_User1` FOREIGN KEY (`User_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_Supplier_User1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
